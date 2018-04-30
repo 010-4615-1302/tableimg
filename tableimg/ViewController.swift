f577bbb0181939dbe4eca4a598445f217ee8eef7
D7703_28 <d7703_28@d7703-28ui-imac.local>
2018-04-30 15:55:55 +0900

    table

--- tableimg/ViewController.swift
+++ tableimg/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var TableView: UITableView!
+    var data = ["고양이", "고양이2", "고양이3"]
+    var subdata = ["11","22","33"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        TableView.dataSource = self
+        TableView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+        //셀 갯수 지정
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let indentifier = "RE"
+        //임시 저장소 셍성
+        let cell = TableView.dequeueReusableCell(withIdentifier: indentifier, for: indexPath)
+        
+        cell.textLabel?.text = data[indexPath.row]
+        cell.detailTextLabel?.text = subdata[indexPath.row]
+        //date,subdate의 안에 있는 값을 텍스트에 출력
+        let myImg = UIImage(named:"\(indexPath.row+1).jpeg")
+        cell.imageView?.image = myImg
+        //이미지출력
+        return cell
+    }
+    
 }
+
+
 
